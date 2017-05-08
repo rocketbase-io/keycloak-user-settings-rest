5a218be8d69ad146f3b94bb5bafab5f93aaad077
Mirco <mirco.franzek@rocketbase.io>
2017-05-08 13:15:24 +0200

    PS-37 PUT Method

--- src/main/java/io/rocketbase/keycloak/usersettings/UserSettingsProvider.java
+++ src/main/java/io/rocketbase/keycloak/usersettings/UserSettingsProvider.java
@@ -4,11 +4,10 @@ import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.UserModel;
 import org.keycloak.services.resource.RealmResourceProvider;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
 
 /**
  * Created by mirco on 05.05.17.
@@ -36,16 +35,34 @@ public class UserSettingsProvider implements RealmResourceProvider {
     }
 
     @GET
-
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public UserWrapper get(@PathParam("id") String id) {
-        UserModel user = session.users()
+        return new UserWrapper(getUser(id));
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response put(@PathParam("id") String id, @FormParam("preferred_username") String userName, @FormParam("given_name") String firstName, @FormParam("family_name") String lastname,
+                        @FormParam("email") String email, @FormParam("avatar") String avatar) {
+        UserModel user = getUser(id);
+        user.setFirstName(firstName);
+        user.setUsername(userName);
+        user.setLastName(lastname);
+        user.setEmail(email);
+        user.setAttribute("avatar", Collections.singletonList(avatar));
+        return Response.ok()
+                .build();
+    }
+
+
+    private UserModel getUser(String id) {
+        return session.users()
                 .getUserById(id,
                         session.getContext()
                                 .getRealm());
-
-        return new UserWrapper(user);
     }
+
 
 }
